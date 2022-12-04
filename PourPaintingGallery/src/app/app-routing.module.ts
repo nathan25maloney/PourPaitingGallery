c792c53707ca274b845099273803359bc97a0302
Nathan Maloney <nathan25maloney@gmail.com>
2022-12-03 21:49:00 -0600

    routing added to the links

--- PourPaintingGallery/src/app/app-routing.module.ts
+++ PourPaintingGallery/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'gallery', component: GalleryComponent},
+  {path: 'gallery/:id', component: GalleryComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
