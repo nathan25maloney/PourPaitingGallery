254a62068589185969d610c66e6355856073e1f9
Nathan Maloney <nathan25maloney@gmail.com>
2022-12-08 22:27:33 -0600

    got the artcard functionality working

--- PourPaintingGallery/src/app/gallery-artcard/gallery-artcard.component.ts
+++ PourPaintingGallery/src/app/gallery-artcard/gallery-artcard.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IArtCard } from '../models/IArtCard';
 
 @Component({
@@ -10,10 +10,30 @@ export class GalleryArtcardComponent implements OnInit {
 
   constructor() { }
 
-  @Input() singleCard!: IArtCard[];
+  @Input() singleCard!: IArtCard;
   
-  orderByLikes() {
-    this.singleCard.sort((a, b) => (a.likes < b.likes) ? -1 : 1);
+  @Output() likesUpdated = new EventEmitter<any>();
+
+  hasVoted = 0;
+
+  incrementLikes() {
+    if (this.hasVoted === -1) {
+      this.singleCard.likes +=2;
+    } else {
+      this.singleCard.likes++;
+    }
+    this.hasVoted = 1;
+    this.likesUpdated.emit(this.singleCard);
+  }
+
+  decrementLikes() {
+    if (this.hasVoted === 1) {
+      this.singleCard.likes -= 2;
+    } else {
+      this.singleCard.likes--;
+    }
+    this.hasVoted = -1;
+    this.likesUpdated.emit(this.singleCard);
   }
 
   ngOnInit(): void {
